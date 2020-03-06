7133fbe482bf377fb9593e9d7af8827828096fd3
GaoMaosen <39178333+maosenGao@users.noreply.github.com>
2020-03-06 16:09:53 +0800

    1

--- eeg/preprocessing/notebooks/deepthought/datasets/eeg/__init__.py
+++ eeg/preprocessing/notebooks/deepthought/datasets/eeg/__init__.py
@@ -1,7 +1,18 @@
-__author__ = 'sstober'
+# __author__ = 'sstober'
 
-# from MultiChannelEEGDataset import MultiChannelEEGDataset
+from MultiChannelEEGDataset import MultiChannelEEGDataset
 
 from MultiChannelEEGSequencesDataset import MultiChannelEEGSequencesDataset
 
 from EEGEpochsDataset import EEGEpochsDataset, DataFile
+
+
+
+# E:\360MoveData\Users\zx305\Documents\GitHub\openmiir\eeg\preprocessing\notebooks\deepthought\datasets\eeg\__init__.py in <module>
+#       3 # from MultiChannelEEGDataset import MultiChannelEEGDataset
+#       4 
+# ----> 5 from MultiChannelEEGSequencesDataset import MultiChannelEEGSequencesDataset
+#       6 
+#       7 from EEGEpochsDataset import EEGEpochsDataset, DataFile
+
+# ModuleNotFoundError: No module named 'MultiChannelEEGSequencesDataset'
